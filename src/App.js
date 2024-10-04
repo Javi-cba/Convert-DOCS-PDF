6c33d876e207d80fdb81103450a5728661ea30eb
Javi-cba <cordobajava@gmail.com>
2024-10-04 14:44:41 -0300

    JC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Iniciocmp from './Iniciocmp';
+
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Iniciocmp />} />
       </Routes>
     </Router>
